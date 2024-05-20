22cc8100a89b4ce5f950e7c8f3c0590c0d567fd8
marcela-celani <marcelacelani.ti@gmail.com>
2024-05-20 10:50:19 -0300

    browser router

--- front-end/src/routes/Router.tsx
+++ front-end/src/routes/Router.tsx
@@ -1,17 +1,17 @@
-import { HashRouter , Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import HomePage from "../pages/HomePage";
 import ErrorPage from "../pages/ErrorPage";
 
 const Router = () => {
   return (
-    <HashRouter >
+    <BrowserRouter >
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/profile" element={<HomePage />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
